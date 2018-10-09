8a4667c5c25455efae7c4bbe09678ac10bc0fe2a
Clyton Dantis <clyton.dantis@gmail.com>
2018-10-09 03:19:49 -0700

    All test cases passed with two sides growing array. Need to do record design

--- rbf/rbfm.cc
+++ rbf/rbfm.cc
@@ -4,77 +4,133 @@
 #include <cmath>
 #include <cstdio>
 #include <cstring>
+#include <iostream>
+#include <ostream>
 
-struct Rinf {
-	unsigned int startPos = 0;
-	unsigned int offset = 0;
+#define SLOT_SIZE sizeof(struct SlotDirectory)
+
+typedef unsigned short int r_slot;
+struct SlotDirectory {
+	r_slot offset = 0;
+	r_slot length = 0;
 };
-/**
- * Assuming that record size ranges from 100 to 1000 bytes.
- * We assume that in 80% of the cases, our record size is 100
- * So the weighted average is 0.8*100 + 0.2*1000 = 280 ~ 256
- * We consider the average record size to be 256 bytes.
- * Therefore, a page of size 4096 bytes will hold 16 (4096/256) records.
- */
-const unsigned RECORDS_PER_PAGE = 16;
 
-struct RecordDictionary {
-	unsigned char numberOfRecords = 0;
+struct PageRecordInfo {
+	r_slot numberOfRecords = 0;
 	/**
 	 * @{code freeSpacePointer} points to the first free space position available in the page
 	 *
 	 */
-	unsigned int freeSpacePos = sizeof(struct RecordDictionary);
-	Rinf recordInfo[RECORDS_PER_PAGE];
+	r_slot freeSpacePos = 0;
 };
+
+/**
+ * Start location of this struct in a page
+ */
+const r_slot PAGE_RECORD_INFO_OFFSET = PAGE_SIZE
+		- sizeof(struct PageRecordInfo);
+
+/**
+ *
+ * @param pri
+ * @return the size taken by array of SlotDirectories and the
+ * 		   PageRecordInfo structure
+ */
+r_slot getRecordDirectorySize(PageRecordInfo pri) {
+	r_slot numberOfRecords = pri.numberOfRecords;
+	return (numberOfRecords * sizeof(struct SlotDirectory) + sizeof(struct PageRecordInfo));
+	cout << "Entire Record Directory size is calculated as"<< numberOfRecords * sizeof(struct SlotDirectory) + sizeof(struct PageRecordInfo)<< endl;
+}
+
+void getPageRecordInfo(PageRecordInfo& pageRecordInfo, char const* pageData) {
+	memcpy(&pageRecordInfo,  (pageData) + PAGE_RECORD_INFO_OFFSET,
+			sizeof(struct PageRecordInfo));
+	cout << "Page Record Info is getting read from"<< 0 + PAGE_RECORD_INFO_OFFSET<< endl;
+}
+
+void putPageRecordInfo(PageRecordInfo &pri, char* pageData) {
+	memcpy( (pageData) + PAGE_RECORD_INFO_OFFSET, &pri,
+			sizeof(struct PageRecordInfo));
+	cout << "Page Record Info is getting added at"<< 0 + PAGE_RECORD_INFO_OFFSET<< endl;
+}
+
+void addSlotDirectory(PageRecordInfo &pri, SlotDirectory &slot, char* pageData) {
+	memcpy(
+			 (pageData) - sizeof(struct SlotDirectory) + PAGE_SIZE
+					- getRecordDirectorySize(pri), &slot,
+			sizeof(struct SlotDirectory));
+	cout << "Slot directory is getting added at"<< 0 - sizeof(struct SlotDirectory) + PAGE_SIZE - getRecordDirectorySize(pri) << endl;
+}
+
+SlotDirectory getSlotForRID(char const* pageData, RID rid, SlotDirectory &slot){
+unsigned int slotNumber = rid.slotNum;
+
+r_slot slotStartPos = PAGE_SIZE - sizeof(struct PageRecordInfo) - sizeof(struct SlotDirectory)*(slotNumber + 1);
+memcpy(&slot, pageData + slotStartPos, sizeof(struct SlotDirectory));
+cout << "Slot for RID slot: " << rid.slotNum << " is : "<< 0 + slotStartPos << endl;
+return slot;
+
+}
+
 /**
  * This method gets the best first page found where a record of size specified
  * by @{code sizeInBytes} will fit.
- * If sizeInBytes is greater than @{code PAGE_SIZE - sizeof(RecordDictionary)}
- * then return PageNum.max
  * If none of the pages in the file can occupy
- * the record, a new page is appended to the file and a RecordDictionary will be
- * created for it. That page number will be returned
+ * the record, a new page is appended to the file and its recordDirectory is
+ * updated for insert
+ *
  * @param fileHandle
  * @param sizeInBytes
+ * @param pageData : pointer pointing to a memory buffer of size PAGE_SIZE
  * @return
  */
-PageNum getPageForRecordOfSize(FileHandle &fileHandle, size_t sizeInBytes) {
-	if (sizeInBytes > (PAGE_SIZE - sizeof(struct RecordDictionary))) {
-		return UINT_MAX; // sentinel value
-	}
+PageNum getPageForRecordOfSize(FileHandle &fileHandle, r_slot sizeInBytes,
+		char* pageData,r_slot &recordNo, r_slot &startPos) {
+//	if (sizeInBytes > (PAGE_SIZE - sizeof(struct SlotDirectory)- sizeof(struct PageRecordInfo))) {
+//		return UINT_MAX; // sentinel value to indicate impossible to add record
+//	}
 	PageNum pageNum;
-	void *data = malloc(PAGE_SIZE);
-	RecordDictionary dict;
 	for (pageNum = 0; pageNum < fileHandle.getNumberOfPages(); pageNum++) {
-		fileHandle.readPage(pageNum, data);
-//         dict = reinterpret_cast<RecordDictionary *> (data);
-		memcpy(&dict, data, sizeof(struct RecordDictionary));
-		unsigned freeSpaceAvailable = PAGE_SIZE - dict.freeSpacePos;
-		if (freeSpaceAvailable > sizeInBytes
-				&& dict.numberOfRecords < RECORDS_PER_PAGE) {
-			free(data);
+		fileHandle.readPage(pageNum, pageData);
+		PageRecordInfo pageRecordInfo;
+		getPageRecordInfo(pageRecordInfo, pageData);
+		r_slot freeSpaceAvailable = PAGE_SIZE
+				- getRecordDirectorySize(pageRecordInfo)//slots + pageRecordInfo
+				- (pageRecordInfo.freeSpacePos); // pg occupied from top
+		if (freeSpaceAvailable > sizeInBytes) {
+			SlotDirectory newSlot;
+			newSlot.offset = pageRecordInfo.freeSpacePos;
+			newSlot.length = sizeInBytes;
+			addSlotDirectory(pageRecordInfo, newSlot, pageData);
+			pageRecordInfo.numberOfRecords++;
+			pageRecordInfo.freeSpacePos+=sizeInBytes;
+			putPageRecordInfo(pageRecordInfo, pageData);
+			recordNo = pageRecordInfo.numberOfRecords - 1;
+			startPos = newSlot.offset;
+			fileHandle.writePage(pageNum, pageData);
 			return pageNum;
 		}
 	}
 	// none of the existing pages can fit the record
 	if (pageNum == fileHandle.getNumberOfPages()) {
-		RecordDictionary dict;
-		memset(data, 0, PAGE_SIZE);
-		memcpy(data, &dict, sizeof(struct RecordDictionary));
-		fileHandle.appendPage(data);
+		SlotDirectory slot;
+		PageRecordInfo pri;
+		slot.offset = 0;
+		slot.length = sizeInBytes;
+		addSlotDirectory(pri, slot, pageData);
+		pri.freeSpacePos = sizeInBytes;
+		pri.numberOfRecords = 1;
+		putPageRecordInfo(pri, pageData);
+		fileHandle.appendPage(pageData);
+		recordNo = pri.numberOfRecords -1 ;
+		startPos = slot.offset;
+		cout << "Appending record in page " << pageNum << "at offset " << startPos << "The length of slot is" << slot.length << endl;
+		cout << "Record Directory Entry: Number Of Records : " << pri.numberOfRecords << endl;
+		cout << "Record Directory Entry: Free Space Pos: " << pri.freeSpacePos << endl;
+//		cout << "Record Directory Entry: Free Space Pos: " << pri.freeSpacePos << endl;
+
 	}
-	free(data);
 	return fileHandle.getNumberOfPages() - 1;
-}
-RC createRecordDictionary(FileHandle &fileHandle) {
-	void *data = malloc(PAGE_SIZE);
-	memset(data, 0, PAGE_SIZE);
-	RecordDictionary *dict = new RecordDictionary;
-	memcpy(data, dict, sizeof(struct RecordDictionary));
-	fileHandle.appendPage(data);
-	free(data);
-	return 0;
 }
 
 RecordBasedFileManager* RecordBasedFileManager::_rbf_manager = 0;
@@ -138,7 +194,7 @@ RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle,
 //	memcpy(nullIndicatorArray, data, nullFieldsIndicatorLength);
 //
 	// Allocate memory for record data to write to page
-	char *record = (char *) malloc(recordSizeInBytes);
+	void *record =  malloc(recordSizeInBytes);
 
 	// copy the nullIndicator bytes into record
 	memset(record, 0, recordSizeInBytes);
@@ -166,57 +222,61 @@ RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle,
 //	}
 
 	// search for a page with free space greater than the record size
-	PageNum pageNum = getPageForRecordOfSize(fileHandle, recordSizeInBytes);
-	if (pageNum == UINT_MAX) {
-//		free(nullIndicatorArray);
-		free(record);
-		return -1; // no insert possible for records greater than page size
-	}
 
-	// Now read the record dictionary and find the first empty slot for insertion
 	char *pageRecordData = (char *) malloc(PAGE_SIZE);
-	RecordDictionary dict;
+	r_slot recordNo;
+	r_slot startPos;
+	PageNum pageNum = getPageForRecordOfSize(fileHandle, recordSizeInBytes, pageRecordData, recordNo, startPos);
 	fileHandle.readPage(pageNum, pageRecordData);
-//	dict = reinterpret_cast<RecordDictionary*>(pageRecordData);
-	memcpy(&dict, pageRecordData, sizeof(struct RecordDictionary));
-	unsigned int newRecordStart = dict.freeSpacePos;
-	unsigned char newRecordNum = dict.numberOfRecords;
-
-	// Then update the dictionary and insert record at freeSpacePointer
-	dict.recordInfo[newRecordNum].startPos = newRecordStart;
-	dict.recordInfo[newRecordNum].offset = recordSizeInBytes;
-	dict.freeSpacePos = newRecordStart + (unsigned int) recordSizeInBytes;
-	dict.numberOfRecords++;
-
-	memcpy(pageRecordData, &dict, sizeof(struct RecordDictionary));
-	memcpy(pageRecordData + newRecordStart, record, recordSizeInBytes);
+	memcpy(pageRecordData + startPos, record, recordSizeInBytes);
 	fileHandle.writePage(pageNum, pageRecordData);
 
+	// Now read the record dictionary and find the first empty slot for insertion
+//	fileHandle.readPage(pageNum, pageRecordData);
+//	dict = reinterpret_cast<RecordDictionary*>(pageRecordData);
+//	memcpy(&dict, pageRecordData, sizeof(struct RecordDictionary));
+//	unsigned int newRecordStart = dict.freeSpacePos;
+//	unsigned char newRecordNum = dict.numberOfRecords;
+//
+//	// Then update the dictionary and insert record at freeSpacePointer
+//	dict.recordInfo[newRecordNum].startPos = newRecordStart;
+//	dict.recordInfo[newRecordNum].offset = recordSizeInBytes;
+//	dict.freeSpacePos = newRecordStart + (unsigned int) recordSizeInBytes;
+//	dict.numberOfRecords++;
+//
+//	memcpy(pageRecordData, &dict, sizeof(struct RecordDictionary));
+//	memcpy(pageRecordData + newRecordStart, record, recordSizeInBytes);
+//	fileHandle.writePage(pageNum, pageRecordData);
+//
 	// update the new rid for the record
 	rid.pageNum = pageNum;
-	rid.slotNum = newRecordNum;
+	rid.slotNum = recordNo;
 	// return 0
 
 	free(record);
+	record = NULL;
 //	free(nullIndicatorArray);
 	free(pageRecordData);
+	pageRecordData = NULL;
 
 	return 0;
 }
 
 RC RecordBasedFileManager::readRecord(FileHandle &fileHandle,
 		const vector<Attribute> &recordDescriptor, const RID &rid, void *data) {
-	void *pageData = malloc(PAGE_SIZE);
+	char *pageData = (char *)malloc(PAGE_SIZE);
 	fileHandle.readPage(rid.pageNum, pageData);
 
-	RecordDictionary dict;
-	memcpy(&dict, pageData, sizeof(struct RecordDictionary));
-
-	Rinf readRecordInfo = dict.recordInfo[rid.slotNum];
-	memcpy(data, (char *) pageData + readRecordInfo.startPos,
-			readRecordInfo.offset);
-
+	SlotDirectory slot;
+	getSlotForRID(pageData, rid, slot);
+	cout << "Reading Data" << endl;
+	cout << "For slot " << rid.slotNum << " : Slot offset from file is "<<  slot.offset << ". And slot length is " << slot.length << endl;
+	PageRecordInfo pri;
+	getPageRecordInfo(pri, (char *)pageData);
+	cout << "Record Directory : Number of records : " << pri.numberOfRecords <<". Free Space : " << pri.freeSpacePos << endl;
+	memcpy(data,  pageData + slot.offset, slot.length);
 	free(pageData);
+	pageData = NULL;
 	return 0;
 }
 
